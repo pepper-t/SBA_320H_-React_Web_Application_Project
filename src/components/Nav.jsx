47f5930e55054d2634987649cfbb55893b8df6dd
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-27 16:17:27 -0500

    Added Nav file from components folder

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,18 @@
 
+
+
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <div className="nav">
+        <Link to="/">
+        <div>Main</div>
+      </Link>
+      <Link to="/library">
+        <div>Real Estate Investor's Library</div>
+      </Link>
+      
+    </div>
+  );
+}
